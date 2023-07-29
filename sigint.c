5c49be32998876f12d33cb454f87485264ed30ab
Salemcity <gemgbeminiyi@gmail.com>
2023-07-29 11:19:43 -0700

    new update

---
+++ sigint.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * get_sigint - handle the crtl + c call in prompt
+ * @sig: signal handler
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
